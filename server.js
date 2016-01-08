d2eeed9a4b32534a666958c6505863e4984a506d
Michael Aufreiter <michael@substance.io>
2016-01-08 15:13:00 +0100

    Pull in Notepad implementation.

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var app = express();
 var port = process.env.PORT || 5000;
 var server = require('substance/util/server');
 
-server.serveStyles(app, '/app.css', path.join(__dirname, 'node_modules', 'substance-examples', 'notepad', 'app.scss'));
-server.serveJS(app, '/app.js', path.join(__dirname, 'node_modules', 'notepad', 'app.js'));
+server.serveStyles(app, '/app.css', path.join(__dirname, 'notepad', 'app.scss'));
+server.serveJS(app, '/app.js', path.join(__dirname, 'notepad', 'app.js'));
 
 // Serve static files
-// app.use(express.static(path.join(__dirname, 'node_modules', 'substance-examples', 'notepad')));
-// app.use('/fonts', express.static(path.join(__dirname, 'node_modules/font-awesome/fonts')));
+app.use(express.static(path.join(__dirname, 'notepad')));
+app.use('/fonts', express.static(path.join(__dirname, 'node_modules/font-awesome/fonts')));
 
 app.listen(port, function() {
   console.log("Lens running on port " + port);
