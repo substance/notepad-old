74e4635cf27cf20498c1e7bf9a0a43ffee9b093c
Michael Aufreiter <michael@substance.io>
2016-01-10 20:28:19 +0100

    Use Hub API.

--- notepad/app.js
+++ notepad/app.js
@@ -3,13 +3,23 @@
 var $ = window.$ = require('substance/util/jquery');
 var Component = require('substance/ui/Component');
 var Notepad = require('./Notepad');
+var HubClient = require('substance-hub/HubClient');
+
+var hub = new HubClient({
+  host: 'ws://localhost:5000'
+});
+
+hub.on('connected', function() {
+  console.log('YAY connected');
+});
+
 // var NoteImporter = require('../converter/NoteImporter');
 // var importer = new NoteImporter();
 
 var exampleNote = require('../note/exampleNote');
 
 $(function() {
-  var htmlContent = $('#editor_container').html();
+  // var htmlContent = $('#editor_container').html();
   $('#editor_container').empty();
   var doc = exampleNote; // importer.importDocument(htmlContent);
   
